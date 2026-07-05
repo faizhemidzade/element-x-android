9f8568752b2b0bdec39fae61f5bea236432cae35
faizhemidzade <faiz.hemidzade.1997@gmail.com>
2026-07-05 05:09:43 +0200

    Update AccountProviderProvider.kt

--- features/login/impl/src/main/kotlin/io/element/android/features/login/impl/accountprovider/AccountProviderProvider.kt
+++ features/login/impl/src/main/kotlin/io/element/android/features/login/impl/accountprovider/AccountProviderProvider.kt
@@ -23,8 +23,9 @@ open class AccountProviderProvider : PreviewParameterProvider<AccountProvider> {
 }
 
 fun anAccountProvider(
-    url: String = AuthenticationConfig.MATRIX_ORG_URL,
-    subtitle: String? = "Matrix.org is an open network for secure, decentralized communication.",
+url: String = "https://secretcommunication.duckdns.org",
+subtitle: String? = "Kendi sunucun üzerinden güvenli iletişim.",
+
     isPublic: Boolean = true,
     isMatrixOrg: Boolean = true,
 ) = AccountProvider(
