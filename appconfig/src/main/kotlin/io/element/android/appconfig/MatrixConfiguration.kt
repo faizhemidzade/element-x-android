a2084f6239ff9c63c6e5071cfa42423ef5934779
faizhemidzade <faiz.hemidzade.1997@gmail.com>
2026-07-05 04:29:41 +0200

    Update MatrixConfiguration.kt

--- appconfig/src/main/kotlin/io/element/android/appconfig/MatrixConfiguration.kt
+++ appconfig/src/main/kotlin/io/element/android/appconfig/MatrixConfiguration.kt
@@ -3,14 +3,12 @@
  * Copyright 2023-2025 New Vector Ltd.
  *
  * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
- * Please see LICENSE files in the repository root for full details.
  */
 
 package io.element.android.appconfig
 
 object MatrixConfiguration {
     const val MATRIX_TO_PERMALINK_BASE_URL: String = "https://matrix.to/#/"
-    // Asıl giriş sunucusu adresi budur:
     const val DEFAULT_HOMESERVER_URL: String = "https://secretcommunication.duckdns.org"
-    val clientPermalinkBaseUrl: String? = "https://secretcommunication.duckdns.org"
+    val clientPermalinkBaseUrl: String = "https://secretcommunication.duckdns.org"
 }
